aa7b153edcfd7d62aae3f759cbfb5e67f46c9f9b
Kyle Harrington <czi@kyleharrington.com>
2023-10-28 14:26:47 -0400

    Adding new/updated com.kyleharrington_n5_utils_0.1.0

--- solutions/com.kyleharrington/n5_utils/solution.py
+++ solutions/com.kyleharrington/n5_utils/solution.py
@@ -25,25 +25,72 @@ def install():
     init_ij()
 
 
+# def run():
+#     from album.runner.api import get_args
+#     from pathlib import Path
+#     from scyjava import config, jimport
+#     import os
+#     import subprocess
+
+#     # This should be OS independent instead, currently macos only
+#     config.add_option('-Djna.library.path=/opt/homebrew/Cellar/c-blosc/1.21.1/lib')
+
+#     config.endpoints.append('org.janelia.saalfeldlab:n5-utils:0.0.7-SNAPSHOT')
+    
+#     View = jimport("org.janelia.saalfeldlab.View")
+#     CommandLine = jimport("picocli.CommandLine")
+
+#     cmd_script = "n5-view"
+    
+#     # Define the arguments
+#     args = [
+#         "-i", "s3://janelia-cosem-datasets/jrc_mus-liver/jrc_mus-liver.n5",
+#         "-d", "/em/fibsem-uint8",
+#         "-r", "16,16,16",
+#         "-c", "0,255",
+#         "-o", "4,4,4",
+#         "-a", "0,2,1",
+#         "-t", "4",
+#         "-s", "1.0,0.5,0.25,0.125"
+#     ]
+
+#     # Combine the command script with its arguments
+#     cmd = [cmd_script] + args
+
+#     # Use subprocess to run the command
+#     result = subprocess.run(cmd, capture_output=True, text=True)
+
+#     # If you want to capture and print stdout or stderr
+#     print(result.stdout)
+#     print(result.stderr)
+
+#     return result
+
 def run():
-    from album.runner.api import get_args
-    from pathlib import Path
-    from scyjava import config, jimport
-    import os
     import subprocess
+    import os
 
-    # This should be OS independent instead, currently macos only
-    config.add_option('-Djna.library.path=/opt/homebrew/Cellar/c-blosc/1.21.1/lib')
+    # Set memory and version specific arguments
+    MEM = 4  # assuming 4 GB, adjust as needed
+    JAVA_VERSION = subprocess.getoutput("java -version 2>&1")
+    GC_OPTION = "-XX:+UseConcMarkSweepGC" if "1.8" in JAVA_VERSION else ""
 
-    config.endpoints.append('org.janelia.saalfeldlab:n5-utils:0.0.7-SNAPSHOT')
-    
-    View = jimport("org.janelia.saalfeldlab.View")
-    CommandLine = jimport("picocli.CommandLine")
+    # Define the JAR path
+    VERSION = "0.0.7-SNAPSHOT"  # Adjust if needed
+    JAR_PATH = os.path.expanduser(f"~/.m2/repository/org/janelia/saalfeldlab/n5-utils/{VERSION}/n5-utils-{VERSION}.jar")
 
-    cmd_script = "n5-view"
-    
+    # Read classpath additions from cp.txt (assuming it exists in the current directory)
+    with open("cp.txt", 'r') as f:
+        cp_additional = f.read().strip()
+
     # Define the arguments
     args = [
+        "java",
+        "-Xmx{}g".format(MEM),
+        GC_OPTION,
+        "-Djna.library.path=/opt/homebrew/Cellar/c-blosc/1.21.1/lib",
+        "-cp", f"{JAR_PATH}:{cp_additional}",
+        "org.janelia.saalfeldlab.View",
         "-i", "s3://janelia-cosem-datasets/jrc_mus-liver/jrc_mus-liver.n5",
         "-d", "/em/fibsem-uint8",
         "-r", "16,16,16",
@@ -54,8 +101,8 @@ def run():
         "-s", "1.0,0.5,0.25,0.125"
     ]
 
-    # Combine the command script with its arguments
-    cmd = [cmd_script] + args
+    # Filter out any empty strings
+    cmd = [x for x in args if x]
 
     # Use subprocess to run the command
     result = subprocess.run(cmd, capture_output=True, text=True)
@@ -65,7 +112,6 @@ def run():
     print(result.stderr)
 
     return result
-
 
 
 setup(
