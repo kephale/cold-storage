f03301abdad7f1ef27bad257c369a672f1588674
Kyle Harrington <czi@kyleharrington.com>
2024-01-27 15:24:32 -0500

    Adding new/updated cryocanvas_setup_demo_0.0.1

--- solutions/cryocanvas/setup_demo/solution.py
+++ solutions/cryocanvas/setup_demo/solution.py
@@ -15,6 +15,7 @@ def download_and_prepare_dataset(client, crop_coords):
     from ome_zarr.io import parse_url
     from ome_zarr.reader import Reader
 
+    import numpy as np
     
     # Select the first tomogram
     tomos = Tomogram.find(client, [Tomogram.tomogram_voxel_spacing.run.dataset.organism_name == "Schizosaccharomyces pombe"])
@@ -32,7 +33,7 @@ def download_and_prepare_dataset(client, crop_coords):
 
    
     # Use the provided cropping coordinates
-    cropped_img = multiscale_img[0, crop_coords[0]:crop_coords[1], crop_coords[2]:crop_coords[3], crop_coords[4]:crop_coords[5]]
+    cropped_img = multiscale_img[0][crop_coords[0]:crop_coords[1], crop_coords[2]:crop_coords[3], crop_coords[4]:crop_coords[5]]
 
     # Save the cropped image into a new Zarr file in the appdirs path
     appdir_path = appdirs.user_data_dir("cryocanvas")
@@ -40,7 +41,7 @@ def download_and_prepare_dataset(client, crop_coords):
         os.makedirs(appdir_path)
     demo_zarr_path = os.path.join(appdir_path, 'demo_dataset.zarr')
     demo_zarr = zarr.open(demo_zarr_path, mode='w')
-    demo_zarr.create_dataset('demo_image', data=cropped_img, chunks=(10, 10, 10))
+    demo_zarr.create_dataset('demo_image', data=np.array(cropped_img), chunks=(10, 10, 10))
 
     return demo_zarr_path
 
@@ -51,8 +52,9 @@ def run():
     from album.runner.api import get_args
     
     # Get cropping coordinates from arguments
-    args = get_args()
-    crop_coords = [args.crop_z_start, args.crop_z_end, args.crop_y_start, args.crop_y_end, args.crop_x_start, args.crop_x_end]
+    # args = get_args()
+    # crop_coords = [args.crop_z_start, args.crop_z_end, args.crop_y_start, args.crop_y_end, args.crop_x_start, args.crop_x_end]
+    crop_coords = [0, 100, 0, 100, 0, 100]
 
     from cryoet_data_portal import Client
     # In your main function or where appropriate
@@ -62,6 +64,8 @@ def run():
         print(f"Demo dataset saved to {demo_dataset_path}")
     except Exception as e:
         print(f"Error: {e}")
+        import traceback
+        traceback.print_exc()
     
 
 setup(
@@ -79,12 +83,12 @@ setup(
     }],
     album_api_version="0.5.1",
     args=[
-        {"name": "crop_z_start", "type": "integer", "required": True, "description": "Start coordinate for cropping on Z axis"},
-        {"name": "crop_z_end", "type": "integer", "required": True, "description": "End coordinate for cropping on Z axis"},
-        {"name": "crop_y_start", "type": "integer", "required": True, "description": "Start coordinate for cropping on Y axis"},
-        {"name": "crop_y_end", "type": "integer", "required": True, "description": "End coordinate for cropping on Y axis"},
-        {"name": "crop_x_start", "type": "integer", "required": True, "description": "Start coordinate for cropping on X axis"},
-        {"name": "crop_x_end", "type": "integer", "required": True, "description": "End coordinate for cropping on X axis"}        
+        {"name": "crop_z_start", "type": "integer", "required": True, "description": "Start coordinate for cropping on Z axis", "default": 0},
+        {"name": "crop_z_end", "type": "integer", "required": True, "description": "End coordinate for cropping on Z axis", "default": 100},
+        {"name": "crop_y_start", "type": "integer", "required": True, "description": "Start coordinate for cropping on Y axis", "default": 0},
+        {"name": "crop_y_end", "type": "integer", "required": True, "description": "End coordinate for cropping on Y axis", "default": 100},
+        {"name": "crop_x_start", "type": "integer", "required": True, "description": "Start coordinate for cropping on X axis", "default": 0},
+        {"name": "crop_x_end", "type": "integer", "required": True, "description": "End coordinate for cropping on X axis", "default": 100}
     ],
     run=run,
     dependencies={
@@ -95,3 +99,5 @@ setup(
         }
     },
 )
+
+
