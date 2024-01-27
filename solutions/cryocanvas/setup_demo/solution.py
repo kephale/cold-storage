19f1e151d1355bbe3a52ae18cec9d636cf0da793
Kyle Harrington <czi@kyleharrington.com>
2024-01-27 14:50:16 -0500

    Adding new/updated cryocanvas_setup_demo_0.0.1

--- solutions/cryocanvas/setup_demo/solution.py
+++ solutions/cryocanvas/setup_demo/solution.py
@@ -90,7 +90,7 @@ setup(
         "parent": {
             "group": "kyleharrington",
             "name": "headless-parent",
-            "version": "0.0.1",
+            "version": "0.0.2",
         }
     },
 )
